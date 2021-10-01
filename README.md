389b61e7bc0a35e13f23586b0b3b2ca1e0160424
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-01 12:21:30 -0400

    establish server & db conection with ES 6 modules

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # learnMEaRN
 
 Following the [JavaScript Mastery](https://www.youtube.com/c/JavaScriptMastery) [Full Stack MERN Project](https://www.youtube.com/playlist?list=PL6QREj8te1P7VSwhrMf3D3Xt4V6_SRkhu) youTube series
+
+While comparing the differences with this tutorial's database set up and the way i've set up mongo databases in the past i came to learn the difference between bringing in modules using CommonJS (requiring module exports) and ES6 (importing exported modules).
+references: [ES 6 modules](https://nodejs.org/docs/latest-v13.x/api/esm.html#esm_enabling), [Strict mode](https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Strict_mode), [export](https://developer.mozilla.org/en-US/docs/web/javascript/reference/statements/export).
--- server/index.js
+++ server/index.js
@@ -1,7 +1,14 @@
 import express from 'express';
-import mongoose from 'mongoose';
+
+import morgan from 'morgan';
+// const morgan = require('morgan');
 import cors from 'cors';
-const dotenv = require('dotenv');
+import dotenv from 'dotenv';
+// const dotenv = require('dotenv');
+import { connectDB } from './config/db.js'
+// const connectDB = require('./config/db');
+
+dotenv.config({ path: '.env'});
 
 const app = express();
 
@@ -13,7 +20,18 @@ app.use(express.json({
   limit: "30mb",
   extended: true
 }));
+
+//cors enables the ability to allow or restrict requested resources on a web server depend on where the HTTP request was initiated. This policy is used to secure a certain web server from access by other website or domain.
 app.use(cors());
 
+if (process.env.NODE_ENV === 'development') {
+  //if development environment use morgan logging middleware 
+  app.use(morgan('dev'));
+}
+
 const CONNECTION_URL = process.env.MONGO_URI;
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
