f45b624907fcae5c7dde7fba6c2294b56bdcc88a
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-01 10:32:15 -0400

    conect to mongo atlas and set up dotenv

--- server/index.js
+++ server/index.js
@@ -1,4 +1,19 @@
 import express from 'express';
-import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+const dotenv = require('dotenv');
+
+const app = express();
+
+app.use(express.urlencoded({
+  limit: "30mb",
+  extended: true
+}));
+app.use(express.json({
+  limit: "30mb",
+  extended: true
+}));
+app.use(cors());
+
+const CONNECTION_URL = process.env.MONGO_URI;
+const PORT = process.env.PORT;
