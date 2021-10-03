886caa146882218cbd5a668b71b6cb01d2583a99
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-03 10:54:28 -0400

    create form

--- client/src/components/Form/styles.js
+++ client/src/components/Form/styles.js
@@ -16,9 +16,10 @@ export default makeStyles((theme) => ({
   },
   fileInput: {
     width: '97%',
-    margin: '10px 0',
+    margin: '10px ',
   },
   buttonSubmit: {
+    marginTop: 10,
     marginBottom: 10,
   },
 }));
