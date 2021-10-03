737f50f192becfd88cf3ed8ccb0f8fabe6484dd1
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-03 12:01:10 -0400

    create Posts lay out

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -3,3 +3,4 @@ import axios from 'axios';
 const url = 'http://localhost:3103/posts';
 
 export const fetchPosts = () => axios.get(url);
+export const createPost = (newPost) => axios.post(url, newPost); 
