5500772ec6b6b6b07550d802ff77f28501cfdb45
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-04 14:39:23 -0400

    Add user specific information to workflow

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'http://localhost:3103/posts';
+const url = 'https://learn-mearn.herokuapp.com/posts';
 
 export const fetchPosts = () => axios.get(url);
 export const createPost = (newPost) => axios.post(url, newPost); 
