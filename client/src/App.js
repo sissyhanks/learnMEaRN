9cc5219f880e37f058542534ba9078053b42dbfd
Bernadette McKnight <sissyhanks@yahoo.com>
2021-10-05 10:47:28 -0400

    create home and auth componemts

--- client/src/App.js
+++ client/src/App.js
@@ -1,41 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import { Container, Grow, Grid } from '@material-ui/core';
+import React from 'react';
+import { Container } from '@material-ui/core';
+import { BrowserRouter, Switch, Route} from 'react-router-dom';
 //following allows us to dispatch an action
-import { useDispatch } from 'react-redux';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
-import { getPosts } from './actions/posts'
-import Posts from './components/Posts/Posts';
-import Form from './components/Form/Form';
 import Navbar from './components/Navbar/Navbar';
+import Home from './components/Home/Home';
+
 
 import useStyles from './styles';
+import Auth from './components/Auth/Auth';
 
 const App = () => {
-  const [currentId, setCurrentId] = useState(null);
-  const classes = useStyles();
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(getPosts());
-  }, [currentId, dispatch]);
 
   return (
-    <Container maxwidth="lg">
-      <Navbar />
-      <Grow in>
-        <Container>
-          <Grid className={classes.mainContainer} container justifyContent="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
-              <Posts setCurrentId={setCurrentId} />
-            </Grid>
-            <Grid item xs={12} sm={4}>
-              <Form currentId={currentId} setCurrentId={setCurrentId} />
-            </Grid>
-          </Grid>
-        </Container>
-      </Grow>
-    </Container>
+    <BrowserRouter>
+      <Container maxwidth="lg">
+        <Navbar />
+        <Switch>
+          <Route path='/' exact component={Home} />
+          <Route path='/auth' exact component={Auth} />
+        </Switch>
+      </Container>
+    </BrowserRouter>
+
   );
 }
 
---
+++ client/src/components/Auth/Auth.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Auth = () => {
+  const state = null;
+
+  return (
+    <div>
+      AUTH
+    </div>
+  )
+}
+
+export default Auth;
